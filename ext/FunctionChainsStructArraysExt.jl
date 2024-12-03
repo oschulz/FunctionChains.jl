ad9054f37fb9ab7257a0b8c2018610fd1a7c5372
Oliver Schulz <oschulz@mpp.mpg.de>
2024-12-03 21:57:01 +0100

    Add StructArrays package extensions

---
+++ ext/FunctionChainsStructArraysExt.jl
@@ -0,0 +1,9 @@
+# This file is a part of FunctionChains.jl, licensed under the MIT License (MIT).
+
+module FunctionChainsStructArraysExt
+
+using StructArrays
+using FunctionChains
+
+
+end # module FunctionChainsStructArraysExt

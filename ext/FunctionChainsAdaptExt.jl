abd48512fc9dacf315e5f19fffb39f3aa3a15750
Oliver Schulz <oschulz@mpp.mpg.de>
2025-07-16 13:29:19 +0200

    STASH falias

--- ext/FunctionChainsAdaptExt.jl
+++ ext/FunctionChainsAdaptExt.jl
@@ -23,4 +23,8 @@ function Adapt.adapt_structure(target::T, ff::FFanout) where T
     FFanout(map(Base.Fix1(Adapt.adapt, target), ffanoutfs(ff)))
 end
 
+function Adapt.adapt_structure(target::T, fa::FAlias) where T
+    falias(Adapt.adapt(target, forig(fa)))
+end
+
 end # module FunctionChainsAdaptExt
